eb67fda3c4d59fe03320a602774880af23d777d3
Yiming <oblin@msn.com>
2016-07-22 17:39:13 +0800

    change settings

--- Entities/FoodDbContext.cs
+++ Entities/FoodDbContext.cs
@@ -4,11 +4,8 @@ namespace OdeToFood.Entities
 {
     public class FoodDbContext : DbContext
     {
-        public FoodDbContext (DbContextOptions<FoodDbContext> options) : base(options)
-        {
-          
-        }
-        
+        public FoodDbContext (DbContextOptions<FoodDbContext> options) : base(options) { }
+
         public DbSet<Restaurant> Restaurants { get; set; }
     }
 }
